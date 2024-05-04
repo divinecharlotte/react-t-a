fc1d206f970081210061ba81f5b41d81fd635337
divine <divinemaina@gmail.com>
2024-05-04 14:21:50 +0200

    rester todoitem tore move the modes

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -13,16 +13,9 @@ const TodoItem = ({ todo, onRemove, onEdit, onChangeStatus }) => {
   useEffect(() => {
     setCurrentLanguage(i18n.language);
   }, [i18n.language]);
-  
-  const [darkMode, setDarkMode] = useState(false);
-  useEffect(() => {
-    setDarkMode(document.documentElement.classList.contains('dark')); // Check if dark mode is enabled in the browser
-  }, []);
-
   let statusColorClass;
   let statusTextColorClass;
-  let listItemBgColorClass;
-
+  
   switch (todo.status) {
     case 'To do':
       statusColorClass = 'bg-orange-100';
@@ -41,16 +34,14 @@ const TodoItem = ({ todo, onRemove, onEdit, onChangeStatus }) => {
       statusTextColorClass = 'text-white';
   }
 
-  listItemBgColorClass = darkMode ? 'bg-blue-500' : 'bg-white';
-
   return (
-    <li className={`flex flex-col gap-4 p-2 rounded-lg ${listItemBgColorClass}`} key={todo.id}>
+    <li className="bg-white flex flex-col gap-4 p-2 rounded-lg " key={todo.id}>
       <p className={`px-2 py-1 rounded-lg w-1/3 ${statusColorClass} ${statusTextColorClass}`}>{t(todo.status)}</p>
-      <h4 className="font-semibold">{t(todo.text)}</h4>
+      <h4 className=" font-semibold">{t(todo.text)}</h4>
       <hr className="bg-slate-300 h-0.5 w-auto" />
       <div className="flex gap-2">
-        <button className="rounded-full text-red-600 bg-red-100 px-1" onClick={() => onRemove(todo.id)}> <FontAwesomeIcon icon={faTrash} /></button>
-        <button className="rounded-full text-orange-600 bg-orange-100 px-1" onClick={() => onEdit(todo.id, todo.text)}><FontAwesomeIcon icon={faPenToSquare} /></button>
+        <button className="rounded-full text-red-600 bg-red-100 px-1 " onClick={() => onRemove(todo.id)}> <FontAwesomeIcon icon={faTrash} /></button>
+        <button className="rounded-full text-orange-600 bg-orange-100  px-1" onClick={() => onEdit(todo.id, todo.text)}><FontAwesomeIcon icon={faPenToSquare} /></button>
         <button className="rounded-full text-green-600 bg-green-100 px-1" onClick={() => onChangeStatus(todo.id, todo.status)}><FontAwesomeIcon icon={faClosedCaptioning} /></button>
       </div>
     </li>
